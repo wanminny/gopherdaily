d9927ed7159b6a44462e8463b41a70b5a7fbe7b7
min.wan <wan.min@zxelec.com>
2024-03-29 13:59:08 +0800

    make deploy IMG=10.200.82.51/k8s/g1:v0.1

--- k8s/operator/default/internal/controller/guestbook_controller.go
+++ k8s/operator/default/internal/controller/guestbook_controller.go
@@ -18,6 +18,7 @@ package controller
 
 import (
 	"context"
+	"k8s.io/klog/v2"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -50,6 +51,7 @@ func (r *GuestbookReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 	_ = log.FromContext(ctx)
 
 	// TODO(user): your logic here
+	klog.Info(req)
 
 	return ctrl.Result{}, nil
 }
